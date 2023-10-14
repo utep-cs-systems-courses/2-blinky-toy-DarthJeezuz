bd9d04f4fe5725b7de32435401fc2ee8e06ddf1f
DarthJeezuz <jjoropeza@miners.utep.edu>
2023-10-14 16:19:38 -0600

    u2

--- project1/sirenMain.c
+++ project1/sirenMain.c
@@ -1,16 +1,20 @@
 #include <msp430.h>
 #include "led.h"
 #include "libTimer.h"
+#include "switches.h"
 #define SW1 BIT3
 #define SW2 BIT4
 #define SW3 BIT5
+#define SWITCHES SW1
 #define RED_LED BIT0
 #define GREEN_LED BIT6
 
 int main(void){
   configureClocks();
   led_init();
-  enableWTDInterrupts();
+  switch_init();
+  enableWDTInterrupts();
 
   or_sr(0x18);
 }
+
